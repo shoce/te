e19a024f8b95b97628ae0c5660713265abbb4c7e
shoce <yogze@icloud.com>
2024-12-01 00:38:25 +0530

    te

---
+++ te.go
@@ -0,0 +1,217 @@
+/*
+history:
+2020/04/17 v1
+
+GoFmt GoBuildNull GoBuild
+*/
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+	"text/template"
+	"time"
+)
+
+func log(msg string, args ...interface{}) {
+	ts := time.Now().Local().Format("Jan/02;15:04")
+	fmt.Fprintf(os.Stderr, ts+" "+msg+"\n", args...)
+}
+
+func Files(fp string) ([]string, error) {
+	var err error
+
+	fi, err := os.Lstat(fp)
+	if err != nil {
+		return nil, err
+	}
+
+	if !fi.Mode().IsDir() {
+		return nil, nil
+	}
+	ff, err := ioutil.ReadDir(fp)
+	if err != nil {
+		return nil, err
+	}
+
+	var names []string
+	for _, i := range ff {
+		if i.Mode()&os.ModeType != 0 {
+			continue
+		}
+		if strings.HasPrefix(i.Name(), ".") {
+			continue
+		}
+		names = append(names, i.Name())
+	}
+
+	return names, nil
+}
+
+func Dirs(fp string) ([]string, error) {
+	var err error
+
+	fi, err := os.Lstat(fp)
+	if err != nil {
+		return nil, err
+	}
+
+	if !fi.Mode().IsDir() {
+		return nil, nil
+	}
+	ff, err := ioutil.ReadDir(fp)
+	if err != nil {
+		return nil, err
+	}
+
+	var names []string
+	for _, i := range ff {
+		if !i.Mode().IsDir() {
+			continue
+		}
+		if strings.HasPrefix(i.Name(), ".") {
+			continue
+		}
+		names = append(names, i.Name())
+	}
+
+	return names, nil
+}
+
+func FilesDirs(fp string) ([]string, error) {
+	ff, err := Files(fp)
+	if err != nil {
+		return nil, err
+	}
+	dd, err := Dirs(fp)
+	if err != nil {
+		return nil, err
+	}
+	rr := append(ff, dd...)
+	return rr, nil
+}
+
+func DirsFiles(fp string) ([]string, error) {
+	dd, err := Dirs(fp)
+	if err != nil {
+		return nil, err
+	}
+	ff, err := Files(fp)
+	if err != nil {
+		return nil, err
+	}
+	rr := append(dd, ff...)
+	return rr, nil
+}
+
+func DirName() (string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", nil
+	}
+	return path.Base(wd), nil
+}
+
+func ReadFile(fp string) (string, error) {
+	bb, err := ioutil.ReadFile(fp)
+	if err != nil {
+		return "", nil
+		//return "", fmt.Errorf("read %s: %s", fp, err)
+	}
+	return string(bb), nil
+}
+
+var (
+	te   *template.Template
+	tefm template.FuncMap
+)
+
+func init() {
+	tefm = template.FuncMap{
+		"Files":       Files,
+		"Dirs":        Dirs,
+		"FilesDirs":   FilesDirs,
+		"DirsFiles":   DirsFiles,
+		"DirName":     DirName,
+		"ReadFile":    ReadFile,
+		"UserHomeDir": os.UserHomeDir,
+		"HasPrefix":   strings.HasPrefix,
+		"HasSuffix":   strings.HasSuffix,
+		"TrimPrefix":  strings.TrimPrefix,
+		"TrimSuffix":  strings.TrimSuffix,
+		"TrimSpace":   strings.TrimSpace,
+	}
+
+	te = template.New("te")
+	te = te.Funcs(tefm)
+}
+
+func main() {
+	var err error
+	var inbb, outbb []byte
+	var in string
+	var inpath, outpath string
+
+	if len(os.Args) > 1 {
+		inpath = os.Args[1]
+	}
+	if len(os.Args) > 2 {
+		outpath = os.Args[2]
+	}
+
+	if inpath != "" {
+		inbb, err = ioutil.ReadFile(inpath)
+		if err != nil {
+			log("read %s: %s", inpath, err)
+			os.Exit(1)
+		}
+	} else {
+		inbb, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log("read stdin: %s", err)
+			os.Exit(1)
+		}
+	}
+	in = string(inbb)
+
+	te, err = te.Parse(in)
+	if err != nil {
+		log("parse: %s", err)
+		os.Exit(1)
+	}
+
+	if outpath != "" {
+		bb := bytes.NewBuffer(nil)
+		err = te.Execute(bb, nil)
+		if err != nil {
+			log("execute: %s", err)
+			os.Exit(1)
+		}
+
+		outbb, err = ioutil.ReadFile(outpath)
+		if err == nil {
+			if bytes.Equal(bb.Bytes(), outbb) {
+				//log("same output as %s, not writing", outpath)
+				os.Exit(0)
+			}
+		}
+
+		err = ioutil.WriteFile(outpath, bb.Bytes(), 0644)
+		if err != nil {
+			log("write %s: %s", outpath, err)
+			os.Exit(1)
+		}
+		log("wrote %d bytes to %s", len(bb.Bytes()), outpath)
+	} else {
+		err = te.Execute(os.Stdout, nil)
+		if err != nil {
+			log("execute: %s", err)
+			os.Exit(1)
+		}
+	}
+}
